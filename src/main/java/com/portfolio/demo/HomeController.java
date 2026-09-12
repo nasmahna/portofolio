604bfebb70cad8ca04f5265c6ed44aecf95d9e05
nasmahna <nasmah466@gmail.com>
2026-09-12 14:33:18 +0700

    add: blog pages and fixing html style

--- src/main/java/com/portfolio/demo/HomeController.java
+++ src/main/java/com/portfolio/demo/HomeController.java
@@ -6,23 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "/linux"})
     public String home() {
-        return "index";
-    }
-
-    @GetMapping("/alt")
-    public String homeSidebar() {
-        return "index-sidebar";
-    }
-
-    @GetMapping("/dayy")
-    public String homeDayy() {
-        return "index-dayy";
-    }
-
-    @GetMapping("/linux")
-    public String homeLinux() {
         return "index-linux";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
     }
 }
